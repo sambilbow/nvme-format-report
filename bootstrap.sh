c1fa9d3a389d20b5e59c66126d8eddcbb152204b
Sam Bilbow <samuel.bilbow@icloud.com>
2025-09-11 18:15:09 +0100

    chore: update bootstrap script to install mise and clone nvme-format-report repository

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,14 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 sudo apt update && sudo apt install gum curl git gum
+
+# Install mise
 curl https://mise.run/bash | sh
 source ~/.bashrc
+
+# Clone the repository
 git clone https://github.com/sambilbow/nvme-format-report.git
+
+# Change to the repository directory
 cd nvme-format-report
 mise setup
